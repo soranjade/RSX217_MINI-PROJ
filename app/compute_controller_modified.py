223bd46c9c79c40dd29196f93da8201c6a454e76
soranjade <anthony-ma@outlook.com>
2025-01-10 14:09:35 +0100

    update disjktra auto path

--- app/compute_controller_modified.py
+++ app/compute_controller_modified.py
@@ -18,7 +18,7 @@ def post_flow(appId,payload):
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
-    print(response.text)
+    #print(response.text)
     
 def rule_to_flow(device_id, port_in, port_out, mac_src, mac_dst):
     # take the rule and convert it into a statefull flow with a txt template file
@@ -175,8 +175,8 @@ class NodeLink:
 links = get_links()
 '''
 for link in links:
-    print(link)
-'''
+    print(link)'''
+
 # {id,mac,locations[{elementId, port}]}
 hosts = get_hosts()
 
@@ -184,11 +184,12 @@ hosts = get_hosts()
 for host in hosts:
     print(host['mac'])
     print(host['locations'][0]['elementId'])
-    print(host['locations'][0]['port'])
-    '''
+    print(host['locations'][0]['port'])'''
+    
 
 nodelink = NodeLink()
 nodelink.links_to_NL(links)
+#print(nodelink)
 nodelink.hosts_to_NL(hosts)
 # result json state of nodelink in json_result/nodelink.json
 #print(json.dumps(nodelink.table))
@@ -197,8 +198,9 @@ nodelink.hosts_to_NL(hosts)
 G = Graph()
 # Convert ONOS links into edge for the graphe
 G.links_to_edge(links)
+print(json.dumps(G.graph))
 '''
-Samples tests to define the shortest path in numerical way
+#Samples tests to define the shortest path in numerical way
 distances_e1, pred_e1 = G.shortest_distances("of:00000000000000e1")
 distances_e2 = G.shortest_distances("of:00000000000000e2")
 distances_e3 = G.shortest_distances("of:00000000000000e3")
@@ -208,13 +210,15 @@ distances_c2 = G.shortest_distances("of:00000000000000c2")
 distances_c3 = G.shortest_distances("of:00000000000000c3")
 distances_c4 = G.shortest_distances("of:00000000000000c4")'''
 
+#print(distances_e1)
 # Example of the shortest path between two nodes E1 -> E3
-#print("path e1 -> e3 : " + str(G.shortest_path("of:00000000000000e1","of:00000000000000e3")))
+#print("path e1 -> e3 : " + str(G.shortest_path("of:00000000000000e1","of:00000000000000e2")))
 
 #print(hosts)
 # HOST TO HOST
+
 h1 = "00:00:00:00:00:01"
-h2 = "00:00:00:00:00:03"
+h2 = "00:00:00:00:00:05"
 
 # GET THE HOST SWITCH CONNECTION
 first_node = nodelink.table[h1]["switch"]
@@ -224,6 +228,7 @@ last_node_port = nodelink.table[h2]["switch_port"]
 
 nodepath = G.shortest_path(first_node,last_node)
 nodepath = json.loads(json.dumps(nodepath))
+print(nodepath)
 
 for node in nodepath:
     index = nodepath.index(node)
@@ -247,9 +252,10 @@ for node in nodepath:
         port_out = nodelink.table[node][nodepath[index+1]]
         
     print(node_id + ", " + port_in + ", " + port_out +", " + mac_src + ", " + mac_dst)
+    post_flow("dijto", json.dumps(json.loads(rule_to_flow(node_id, port_in, port_out, mac_src, mac_dst))))
     
 #print("path e1 -> e3 : " + str(nodepath))
-
+'''
 
 flows = [ 
          ["of:00000000000000e1", "1", "3", "00:00:00:00:00:01", "00:00:00:00:00:03"],
@@ -261,4 +267,4 @@ flows = [
 for flow in flows :
     #print(flow)
     device_id, port_in, port_out, mac_src, mac_dst = flow
-    post_flow("dijto", json.dumps(json.loads(rule_to_flow(device_id, port_in, port_out, mac_src, mac_dst))))
+    post_flow("dijto", json.dumps(json.loads(rule_to_flow(device_id, port_in, port_out, mac_src, mac_dst))))'''
